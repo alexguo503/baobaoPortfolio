3f57118146c2ad085acfe76c62dd02d7520579ac
alexguo503 <alexguo503@163.com>
2023-03-08 10:12:22 +0800

    提交信息

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['source.unsplash.com', 'www.nps.gov', 'images.unsplash.com', 'assets.codepen.io', 'dummyimage.com'],
+    domains: ['source.unsplash.com', 'www.nps.gov', 'images.unsplash.com', 'assets.codepen.io', 'dummyimage.com', 'localhost'],
     // deviceSizes: [640, 750, 828.1080, 1200, 1920, 2048, 3840],
     // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     // minimumCacheTTL: 60,
