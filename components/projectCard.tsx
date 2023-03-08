3af886cd50798a1c5a68b272925d71c940d70ef9
alexguo503 <alexguo503@163.com>
2023-03-08 11:38:33 +0800

    update更新

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -1,7 +1,7 @@
 // 这个文件不能用 ctrl+s 做 prettier 代码格式美化会报错?
 
 import { FC } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 
 import { Project } from "../pages/api/data";
 
@@ -12,41 +12,58 @@ const ProjectCard: FC<{
   project: Project;
   showDetail: string | null;
   setShowDetail: (id: null | string) => void;
-}> = ({
-  project: {
-    id,
-    name,
-    image_path,
-  },
-  showDetail,
-  setShowDetail,
-}) => {
+}> = ({ project: { id, name, image_path }, showDetail, setShowDetail }) => {
   return (
     <>
       <div className="relative flex flex-col items-center">
-        <Image
+        {/* <Image
           src={image_path}
           alt={name}
           width={300}
           height={150}
           className="cursor-pointer rounded-md"
           onClick={() => setShowDetail(id)}
+        /> */}
+
+        <motion.img
+          className="cursor-pointer rounded-md"
+          alt={name}
+          src={image_path}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1 }}
+          onClick={() => setShowDetail(id)}
         />
-        <p className="my-2 text-center font-bold text-gray-800 dark:text-white">{name}</p>
+        <p className="my-2 text-center font-bold text-gray-800 dark:text-white">
+          {name}
+        </p>
       </div>
 
       {showDetail === id && (
         <div className="fixed top-0 left-0 w-full h-full bg-gray-900 place-content-start grid z-20">
-              <motion.div className="fixed top-0 left-0 w-full h-full z-10 grid place-content-center" variants={fadeInUp}  onClick={() => setShowDetail(null)}>
-                <Image
+          <motion.div
+            className="fixed top-0 left-0 w-full h-full z-10 grid place-content-center"
+            variants={fadeInUp}
+            onClick={() => setShowDetail(null)}
+          >
+            {/* <Image
                   src={image_path}
                   alt={name}
                   width={1000}
                   height={600}
                   className="rounded-md"
-                />
-              </motion.div>
-          </div>
+                /> */}
+
+            <motion.img
+              className="cursor-pointer rounded-md"
+              alt={name}
+              src={image_path}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
+          </motion.div>
+        </div>
       )}
     </>
   );
